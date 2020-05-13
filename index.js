a860a515106c12eb545024e0c064fb235cef8bc9
Ashlee Barksdale <ashleelennab@gmail.com>
2020-05-13 22:42:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+var output = [];
+for (var i = 0; i < dwarves.length ; i++) {
+  output.push(`${i + 1}. ${dwarves[i]} `)
+}
+return output.join("")
 }
 
 function summonCaptainPlanet(planeteerCalls){
+return planeteerCalls.map(a=>a.toUpperCase()+"!")
+  
 }
 
 function longPlaneteerCalls(words) {
+  for(var i = 0 ; i<words.length ; i++){
+    if (words[i].length>4){
+      return true
+    }
+  }
+  return false
 }
 
 function findTheCheese (foods) {
+  
+  for(var i = 0 ; i<foods.length ; i++){
+  if (foods[i]=="cheddar"
+  ||foods[i]=="gouda"
+  ||foods[i]=="camembert"
+  ){
+    return foods[i]
+  }
+}
+return "no cheese!"
 }
